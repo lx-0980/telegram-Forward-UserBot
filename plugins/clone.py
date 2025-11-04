c6810dbb51b2f8181c461b5455b17e9289d481b9
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-04 19:07:38 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -177,7 +177,7 @@ async def forward_files(lst_msg_id, chat, msg, bot, user_id):
                 await msg.edit(f"Successfully Forward Canceled!")
                 FORWARDING[user_id] = False 
                 break
-            if forwarded == 500:
+            if forwarded == 5000:
                 await msg.edit(f"Forward stopped! You Reached Max Limit\n<b>Message ID</b>: <code>{message.id}</code>\n<b>Forwarded</b>: <code>{forwarded}</code>")
                 FORWARDING[user_id] = False 
                 break
@@ -249,7 +249,7 @@ async def forward_files(lst_msg_id, chat, msg, bot, user_id):
                 logger.exception(e)
                 return await msg.reply(f"Forward Canceled!\n\nError - {e}")               
             forwarded += 1
-            await asyncio.sleep(4)            
+            await asyncio.sleep(7)            
     except Exception as e:
         logger.exception(e)
         await msg.reply(f"Forward Canceled!\n\nError - {e}")
